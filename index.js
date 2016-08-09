dc067a585e53184069e668f7ad86683c0ab3c043
Heriberto Basulto <basulto93@live.com>
2016-08-09 08:45:39 -0700

    Added a map, working on adding markers to the map next.

--- index.js
+++ index.js
@@ -8,8 +8,20 @@ app.controller('SDTopSpots', function($scope, $http){
 	.then(function (response){
 		$scope.spot = response.data;});
 		console.log("worked");
+
+		var mymap = L.map('mapid').setView([32.726008, -117.1655245], 12);
+			L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+		    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+		    maxZoom: 18,
+		   	id: 'basulto93.130fejo7',
+		   	accessToken: 'pk.eyJ1IjoiYmFzdWx0bzkzIiwiYSI6ImNpcm5saGY3cTA5c2xmbG04eXBseXJlNG8ifQ.PTCv5lO31gSM8B_eVEZ0sw'
+			}).addTo(mymap);
 	//Just another way to navigate to another page, adding ng-click="openGoogleMaps(spot) would make this work
 	$scope.openGoogleMaps = function (spot) {
 		window.location.replace('https://maps.google.com?q=' + spot.location[0] + ',' + spot.location[1]);
 	};
 });
+
+
+
+
